f1556583cb63f132d7c44a12f9b440b0e37e4a2e
Wes Doyle <wesdoyle08@gmail.com>
2018-12-08 12:29:25 -0600

    Add service files

---
+++ services/log-service/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name='petrichor-log-service', version='0.0.1', packages=find_packages())
--- services/weather-service/setup.py
+++ services/weather-service/setup.py
@@ -1,3 +1,3 @@
-from setup.py import setup, find_packages
+from setuptools import setup, find_packages
 
-setup(name='weather-api', version='0.0.1', packages=find_packages())
+setup(name='petrichor-weather-service', version='0.0.1', packages=find_packages())
