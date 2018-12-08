be8cb0af47268a33ae4ec71cd7541f0ac2106c91
Wes Doyle <wesdoyle08@gmail.com>
2018-12-08 12:56:21 -0600

    Update copy script

--- services/pypi-service/copy_build_artifacts.py
+++ services/pypi-service/copy_build_artifacts.py
@@ -1,11 +1,22 @@
-import os
 import glob
+import os
+from distutils.core import run_setup
 from shutil import copyfile, SameFileError
 
 PETRICHOR_HOME = os.environ.get("PETRICHOR_HOME")
+SETUP_FILES = PETRICHOR_HOME + '/**/*setup.py'
+TARBALLS = PETRICHOR_HOME + '/**/*tar.gz'
 
-for filename in glob.iglob(PETRICHOR_HOME + '/**/*tar.gz', recursive=True):
-    print(f"Exploring {PETRICHOR_HOME} for python packages")
+for filename in glob.iglob(SETUP_FILES, recursive=True):
+    print(f"Step 1: Exploring {PETRICHOR_HOME} for setup files")
+    run_setup(
+            filename,
+            # script_args=sys.argv[1:],
+            stop_after='run'
+    )
+
+for filename in glob.iglob(TARBALLS, recursive=True):
+    print(f"Step 2: Exploring {PETRICHOR_HOME} for python packages")
     if "site-packages" not in filename:
         head, fname = os.path.split(filename)
         dest = os.path.join(PETRICHOR_HOME, "packages", "python", fname)
