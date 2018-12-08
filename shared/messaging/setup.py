e7821ba4f21d76e7d998a192262a02cab2365bd2
Wes Doyle <wesdoyle08@gmail.com>
2018-12-08 09:48:57 -0600

    Update packages

--- shared/messaging/setup.py
+++ shared/messaging/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-        name="petrichor-requester",
+        name="petrichor-messaging",
         version="0.0.1",
         packages=find_packages(),
         install_requires=['requests']
