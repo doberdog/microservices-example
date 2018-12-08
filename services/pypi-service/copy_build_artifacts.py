214c39a7ee45becaba9bbf15eb853ce7ad92f722
Wes Doyle <wesdoyle08@gmail.com>
2018-12-08 12:49:56 -0600

    Update copy script

--- services/pypi-service/copy_build_artifacts.py
+++ services/pypi-service/copy_build_artifacts.py
@@ -5,11 +5,12 @@ from shutil import copyfile, SameFileError
 PETRICHOR_HOME = os.environ.get("PETRICHOR_HOME")
 
 for filename in glob.iglob(PETRICHOR_HOME + '/**/*tar.gz', recursive=True):
+    print(f"Exploring {PETRICHOR_HOME} for python packages")
     if "site-packages" not in filename:
         head, fname = os.path.split(filename)
         dest = os.path.join(PETRICHOR_HOME, "packages", "python", fname)
         try:
-            print("Copying [{}] to: {}".format(fname, dest))
+            print(f"Copying [{fname}] to: {dest}")
             copyfile(filename, dest)
         except SameFileError:
             pass
