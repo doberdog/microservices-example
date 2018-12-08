0ca020734d02c61d9e27fd32f789984bc8c3e6ae
Wes Doyle <wesdoyle08@gmail.com>
2018-12-07 20:19:34 -0600

    Add weather-service.py

---
+++ services/weather-api/weather-service.py
@@ -0,0 +1,11 @@
+"""
+The WeatherService invokes a web API to provide data about the weather
+"""
+
+
+class WeatherService:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
