17afa8df1f5e6e333b97b7dadd5c88aeddf55167
Wes Doyle <wesdoyle08@gmail.com>
2018-12-07 19:48:44 -0600

    Update copy build artifacts sh

--- services/pypi-service/copy_build_artifacts.py
+++ services/pypi-service/copy_build_artifacts.py
@@ -2,9 +2,9 @@ import os
 import glob
 from shutil import copyfile, SameFileError
 
-RR_HOME = os.environ.get("RR_HOME")
+SERVICES_HOME = os.environ.get("SERVICES_HOME")
 
-for filename in glob.iglob(RR_HOME + '/riverrun/**/*tar.gz', recursive=True):
+for filename in glob.iglob(RR_HOME + '/services/**/*tar.gz', recursive=True):
     if "site-packages" not in filename:
         head, fname = os.path.split(filename)
         dest = os.path.join(RR_HOME, "packages", "python", fname)
