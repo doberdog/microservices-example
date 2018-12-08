e27d12c5c21a5f9082159b8fff6a1604f4672a92
Wes Doyle <wesdoyle08@gmail.com>
2018-12-08 13:46:27 -0600

    Clean

--- services/weather-service/run.sh
+++ services/weather-service/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 pip install -e .
-pip install -r local_packages.txt --extra-index-url http://layers:8081/ --trusted-host layers
+pip install -r local_packages.txt --extra-index-url http://petricnet:8081/ --trusted-host petricnet
 
 python -u ./weather-service/weather-service.py
