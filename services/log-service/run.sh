42cd89b3e0a282718f86474521cf65f5f6b1a7b9
Wes Doyle <wesdoyle08@gmail.com>
2018-12-09 16:08:45 -0600

    Update scripts

--- services/log-service/run.sh
+++ services/log-service/run.sh
@@ -2,5 +2,5 @@
 pip install -e .
 
 pip install -r local_packages.txt --extra-index-url http://pypi-server:8081/ --trusted-host pypi-server
-python -u ./log-service/log_service.py
+python -u ./petrichor_log_service/log_service.py
 
--- services/weather-service/run.sh
+++ services/weather-service/run.sh
@@ -2,4 +2,4 @@
 pip install -e .
 
 pip install -r local_packages.txt --extra-index-url http://pypi-server:8081/ --trusted-host pypi-server
-python -u ./weather-service/weather_service.py
+python -u ./petrichor_weather_service/weather_service.py
