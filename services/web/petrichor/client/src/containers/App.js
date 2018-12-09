1ff6463649927195bd4a54e7652fabd69d1c9a98
Wes Doyle <wesdoyle08@gmail.com>
2018-12-09 17:46:05 -0600

    Update structure

---
+++ services/web/petrichor/client/src/containers/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './App.css';
+import ZipCodeForm from "../components/ZipCodeForm";
+
+class App extends Component {
+    state = {
+        response: '',
+        post: '',
+        responseToPost: '',
+    };
+
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({ response: res.express }))
+            .catch(err => console.log(err));
+    }
+
+    callApi = async () => {
+        const response = await fetch('/api/hello');
+        const body = await response.json();
+
+        if (response.status !== 200) throw Error(body.message);
+
+        return body;
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch('/api/world', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ post: this.state.post }),
+        });
+        const body = await response.text();
+
+        this.setState({ responseToPost: body });
+    };
+
+    render() {
+        return (
+            <div className="app">
+                <div className="content">
+                    <div className="heading">
+                        Get the Weather
+                    </div>
+                    <div className='form-content'>
+                        <ZipCodeForm/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
