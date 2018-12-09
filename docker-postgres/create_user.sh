965e9edf8196d97101ca6a938e63c0aeab170ad8
Wes Doyle <wesdoyle08@gmail.com>
2018-12-09 15:45:01 -0600

    Remove unused

--- docker-postgres/create_user.sh
+++ docker-postgres/create_user.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-POSTGRES="psql --username ${POSTGRES_USER}"
+POSTGRES="psql --username postgres"
 
-echo "Creating database role: ${APP_USER}"
+echo "Creating database role: petrichor"
 
-$POSTGRES <<-EOSQL
-CREATE USER ${APP_USER} WITH CREATEDB PASSWORD '${APP_USER_PASS}';
+${POSTGRES} <<-EOSQL
+CREATE USER petrichor WITH PASSWORD 'petrichor';
 EOSQL
