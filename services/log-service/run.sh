d8f39ca1133872db3661182bf5fef9544c920d20
Wes Doyle <wesdoyle08@gmail.com>
2018-12-09 15:07:57 -0600

    Renames

--- services/log-service/run.sh
+++ services/log-service/run.sh
@@ -2,4 +2,5 @@
 pip install -e .
 
 pip install -r local_packages.txt --extra-index-url http://pypi-server:8081/ --trusted-host pypi-server
-python -u start.py
+python -u ./log-service/log_service.py
+
--- services/weather-service/run.sh
+++ services/weather-service/run.sh
@@ -2,4 +2,4 @@
 pip install -e .
 
 pip install -r local_packages.txt --extra-index-url http://pypi-server:8081/ --trusted-host pypi-server
-python -u ./weather-service/weather-service.py
+python -u ./weather-service/weather_service.py
