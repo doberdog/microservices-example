2930d2f5e44728668075f8bee1ceeaca59b019e9
Wes Doyle <wesdoyle08@gmail.com>
2018-12-09 14:09:26 -0600

    Fixes

--- services/weather-service/run.sh
+++ services/weather-service/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 pip install -e .
 
-pip install -r local_packages.txt --extra-index-url http://petricnet:8081/ --trusted-host petricnet
+pip install -r local_packages.txt --extra-index-url http://pypi-server:8081/ --trusted-host pypi-server
 python -u ./weather-service/weather-service.py
